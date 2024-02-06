75926ec3a0000bf81ed66670faa60f922cd44e00
aydanesedova <sd4d2n2ah@code.edu.az>
2024-02-07 01:45:02 +0400

    fake-api

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,41 +1,39 @@
-let bestSeller = document.getElementById("best_seller")
-let seeMore = document.getElementById("see-more")
+
+let bestSeller = document.getElementById("best_seller");
+let seeMore = document.getElementById("see-more");
+
 
 let page = 1;
 let limit = 3;
 
+const renderData = async () => {
+  
+  const res = await fetch(
+    `http://localhost:3000/posts`
+  );
+  const data = await res.json();
+  console.log(data);
+  data.forEach((item) => {
+    let miniDiv = document.createElement("div");
+    miniDiv.className = "miniDiv";
+    miniDiv.innerHTML = `
+                        <a href="${item.link}" target="_blank">
+                         <img src="${item.image}" alt="${item.name}">
+                         <div class="markaName">
+                         </a>
 
-const renderProducts = (page, limit) => {
-    axios.get(`http://localhost:3000/posts?_limit=${limit}_page=${page}`)
-        .then((response) => {
-            const posts = response.data;
-            bestSeller.innerHTML = "";
-            posts.forEach((post) => {
-                const { id, image, name, price, marka } = post;
-                let miniDiv = document.createElement("div");
-                miniDiv.className = "miniDiv";
-                miniDiv.innerHTML = `
-                    <a href="https://www.trendyol.com/?id=${id}">
-                        <img src="${image}" alt="${name}">
-                   <div class="markaName">  
-                   <p>${marka}</p>
-                   <h4>${name}</h4>
-                   </div>
-                   <p class= "lowerPrice"><i class="fa-regular fa-arrow-trend-down"></i> Son 30 Günün En Düşük Fiyatı!</p>
-                   <p>${price} TL</p>
-                    </a>
-                `;
-                bestSeller.append(miniDiv);
-            });
-        })
-        .catch((error) => {
-            console.error('Bir hata oluştu:', error);
-        });
-}
+
+    `;
+    bestSeller.append(miniDiv);
+  });
+  // page++;
+};
+
+seeMore.addEventListener("click", renderData);
 
 window.onload = () => {
-    renderProducts(page, limit);
-}
+  renderData();
+};
 
 
-seeMore.addEventListener("click",renderProducts)
+
